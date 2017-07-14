a127422682fb61f2c1eeb836bae1bd0411ac4028
Jan-Luca Berger <janluca@officeconduct.com>
2017-07-14 16:14:33 -0400

    pass specs for lru cache

--- hash_map/lib/p06_lru_cache.rb
+++ hash_map/lib/p06_lru_cache.rb
@@ -1,9 +1,9 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
-
+require 'byebug'
 class LRUCache
-  attr_reader :count
-  def initialize(max, prc)
+  attr_accessor :count, :map, :store, :prc, :max
+  def initialize(max, prc = Proc.new { |x| x ** 2 })
     @map = HashMap.new
     @store = LinkedList.new
     @max = max
@@ -11,10 +11,20 @@ class LRUCache
   end
 
   def count
-    @map.count
+    map.count
   end
 
   def get(key)
+    if map.include?(key)
+      update_link!(map[key])
+      return map[key].val
+    else
+      eject! if count == max
+      value = prc.call(key)
+      self.store.append(key, value)
+      self.map[key] = self.store.last
+      value
+    end
   end
 
   def to_s
@@ -28,9 +38,17 @@ class LRUCache
   end
 
   def update_link!(link)
-    # suggested helper method; move a link to the end of the list
+    key = link.key
+    val = link.val
+    self.store.remove(key)
+    self.store.append(key, val)
+    map[key] = store.first
   end
 
   def eject!
+    new_first = store.first.next
+    map.delete(store.first.key)
+    store.head.next = new_first
+    new_first.prev = store.head
   end
 end
