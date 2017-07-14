b5baee20b8c45cb61002a8cb402f038e1b0967a9
Jan-Luca Berger <janluca@officeconduct.com>
2017-07-14 17:32:30 -0400

    Inital attempt at dynamic arrays

--- hash_map/lib/p07_dynamic_array_bonus.rb
+++ hash_map/lib/p07_dynamic_array_bonus.rb
@@ -17,25 +17,34 @@ class StaticArray
     @store.length
   end
 
+  def render
+    @store
+  end
+
   private
 
   def validate!(i)
+    # puts @store.length
     raise "Overflow error" unless i.between?(0, @store.length - 1)
   end
 end
 
 class DynamicArray
-  attr_reader :count
+
+  attr_accessor :store, :count, :temp_store
 
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
     @count = 0
+    @temp_store = nil
   end
 
   def [](i)
+    self.store[i]
   end
 
   def []=(i, val)
+    self.store[i] = val
   end
 
   def capacity
@@ -46,9 +55,40 @@ class DynamicArray
   end
 
   def push(val)
+    resize! if count == store.length
+    self.store[count] = val
+    self.count += 1
   end
 
+  # def unshift(val)
+  #   resize! if count == store.length
+  #   i = 0
+  #   next_val = store[0]
+  #   while i < self.store.length
+  #     self.store[i] = val
+  #     i += 1
+  #     break if i >= store.length
+  #     val = next_val
+  #     if i < store.length - 1
+  #       next_val = store[i]
+  #     end
+  #   end
+  # end
+
   def unshift(val)
+    temp_store = DynamicArray.new(self.store.length)
+      puts "#{temp_store.store.render}"
+    temp_store.store[0] = val
+      puts "#{temp_store.store.render}"
+    i = 1
+    while i < count - 1
+
+      temp_store.push(self.store[i])
+      puts "#{temp_store.store.render}"
+
+      i += 1
+    end
+    puts "#{temp_store.store.render}"
   end
 
   def pop
@@ -63,7 +103,13 @@ class DynamicArray
   def last
   end
 
-  def each
+  def each(&prc)
+    i = 0
+    while i < self.store.length
+      prc.call(self.store[i])
+      i += 1
+    end
+    self
   end
 
   def to_s
